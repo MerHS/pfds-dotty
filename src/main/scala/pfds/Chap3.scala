f0020059bb8547d2c2e09b1d92df1988c7c8e70e
KINETC <starvessel@naver.com>
2019-12-22 16:19:41 +0900

    Chap3 factories

--- src/main/scala/pfds/Chap3.scala
+++ src/main/scala/pfds/Chap3.scala
@@ -2,24 +2,53 @@ package net.kinetc.pfds
 import scala.math.Ordering
 import scala.annotation.tailrec
 
-import net.kinetc.pfds.Util.given
-
 object Chap3 {
-  trait Heap[+Elem: Ordering, T[+_]] {
-    def empty: T[Nothing]
-    def isEmpty: Boolean
+  trait HeapFactory[T[A] <: Heap[A, T, T[A]]] {
+    def empty[A]: T[A]
 
-    def merge[A >: Elem : Ordering](that: T[A]): T[A]
-    def insert[A >: Elem : Ordering](a: A): T[A]
-    
-    def findMin: Elem
-    def deleteMin: T[Elem]
+    @tailrec
+    final def formHelper[A: Ordering](l: Iterator[T[A]]): Iterator[T[A]] = {
+      if (!l.hasNext)  {
+        return Iterator.empty
+      }
+      val fst = l.next
+      if (!l.hasNext)  {
+        return Iterator(fst)
+      }
+      val snd = l.next
+      formHelper(Iterator(fst.merge(snd)) ++ l.sliding(2, 2).map({
+        _.toList match {
+          case t1 :: t2 :: Nil => 
+            t1.merge(t2)
+          case t :: Nil => t
+          case _ => empty
+        }
+      }))
+    }
+
+    def formList[A: Ordering](l: IterableOnce[A]): T[A] = {
+      var iter = formHelper(l.iterator.map(empty.insert(_)))
+      if (iter.hasNext) {
+        iter.toList.head
+      } else {
+        empty
+      }
+    }
   }
 
-  sealed abstract class LeftistHeap[+Elem: Ordering] extends Heap[Elem, LeftistHeap] {
+  trait Heap[+Elem, CC[_], +C] {
+    def isEmpty: Boolean
+
+    def merge[A >: Elem : Ordering](that: CC[A]): CC[A]
+    def insert[A >: Elem : Ordering](a: A): CC[A]
+    
+    def findMin: Elem
+    def deleteMin: C
+  }
+
+  sealed abstract class LeftistHeap[+Elem: Ordering] extends Heap[Elem, LeftistHeap, LeftistHeap[Elem]] {
     import LeftistHeap._
 
-    override def empty = LHEmpty
     override def isEmpty = this == LHEmpty
 
     override def merge[A >: Elem : Ordering](that: LeftistHeap[A]): LeftistHeap[A] = {
@@ -53,9 +82,20 @@ object Chap3 {
       case LHEmpty => 0
       case LHNode(r, _, _, _) => r
     }
+
+    // ex 3.2
+    def insertInline[A >: Elem : Ordering](a: A): LeftistHeap[A] = 
+      this match {
+        case LHEmpty => LHNode(1, a, LHEmpty, LHEmpty)
+        case LHNode(_, x, l, r) =>
+          if (summon[Ordering[A]].lt(x, a))
+            makeT(x, l, r.insertInline(a))
+          else
+            makeT(a, l, r.insertInline(x))
+      }
   }
 
-  object LeftistHeap {
+  object LeftistHeap extends HeapFactory[LeftistHeap] {
     case class HeapEmptyException() extends Exception()
 
     case object LHEmpty extends LeftistHeap[Nothing]
@@ -63,6 +103,8 @@ object Chap3 {
     case class LHNode[Elem: Ordering](
       rankVal: Int, elem: Elem, lc: LeftistHeap[Elem], rc: LeftistHeap[Elem]
     ) extends LeftistHeap[Elem]
+
+    def empty[A] = LHEmpty
 
     def makeT[Elem: Ordering](
       elem: Elem, h1: LeftistHeap[Elem], h2: LeftistHeap[Elem]
--- src/main/scala/pfds/Util.scala
+++ src/main/scala/pfds/Util.scala
@@ -3,7 +3,5 @@ package net.kinetc.pfds
 import scala.math.Ordering
 
 object Util {
-  given Ordering[Nothing] {
-    def compare(a: Nothing, b: Nothing) = 0
-  }
+  
 }
